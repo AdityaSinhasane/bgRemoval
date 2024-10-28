23327877c467d8787fb34a3cef2052ac1198ba0b
AdityaSinhasane <adityasinhasane100@gmail.com>
2024-10-28 21:59:58 +0530

    First Commit

---
+++ client/src/assets/assets.js
@@ -0,0 +1,85 @@
+import logo from './logo.svg'
+import logo_icon from './logo_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import header_img from './header_img.png'
+import remove_bg_icon from './remove_bg_icon.svg'
+import upload_btn_icon from './upload_btn_icon.svg'
+import upload_icon from './upload_icon.svg'
+import download_icon from './download_icon.svg'
+import image_w_bg from './image_w_bg.png'
+import image_wo_bg from './image_wo_bg.png'
+import facebook_icon from './facebook_icon.svg'
+import google_plus_icon from './google_plus_icon.svg'
+import twitter_icon from './twitter_icon.svg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import credit_icon from './credit_icon.png'
+
+export const assets = {
+    logo,
+    logo_icon,
+    arrow_icon,
+    header_img,
+    remove_bg_icon,
+    upload_icon,
+    download_icon,
+    image_w_bg,
+    image_wo_bg,
+    facebook_icon,
+    google_plus_icon,
+    twitter_icon,
+    upload_btn_icon,
+    credit_icon
+}
+
+export const testimonialsData = [
+    {
+        id: 1,
+        text: "I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.",
+        author: "Omkar Chavan",
+        image: "https://media.licdn.com/dms/image/v2/D4D35AQFchAQC_nRpbw/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1719407150200?e=1730649600&v=beta&t=UAGm3wS24JN5HcFcgo0xqeU4rv9J041FYz8VP7dT8cM",
+        jobTitle:'Web Developer at Qspider'
+    },
+    {
+        id: 2,
+        text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
+        author: "Swaraj Pawar",
+        image: "https://media.licdn.com/dms/image/v2/D4D03AQEDrcugKZb4_g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1691856550284?e=1735776000&v=beta&t=0rHzx5CEmF9Uh2FjUKBU0XoFjR56gIkYxNA5MaMmAzU",
+        jobTitle:'Software Developer at PTC'
+    },
+    {
+      id: 2,
+      text: "This app is like magic! It removes backgrounds perfectly every time. I’ll never use anything else.",
+      author: "Sheryash Patil",
+      image: "https://media.licdn.com/dms/image/v2/D4D35AQHRqF2G-R29Tw/profile-framedphoto-shrink_100_100/profile-framedphoto-shrink_100_100/0/1720074804317?e=1730649600&v=beta&t=SxhCQhqGpJvpTwRyyyKJK0V_F6yVdA723haBi9Oy1Ak",
+      jobTitle:'Founder of DESports'
+  },
+  {
+    id: 2,
+    text: "It’s so simple, anyone can use it! Just click, and the background’s gone in a second. Perfect for when you’re in a rush.",
+    author: "Ayush Phapale",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQEgof5h0Sy5ug/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1672769031651?e=1735776000&v=beta&t=Hx6vMJZMQs4cyM0l0CNEF3TudQjsG3O4KLRkeJ5h-dY",
+    jobTitle:'Software Developer at Qspider'
+},
+];
+
+export const plans = [
+    {
+      id: 'Basic',
+      price: 10,
+      credits: 100,
+      desc: 'Best for personal use.'
+    },
+    {
+      id: 'Advanced',
+      price: 50,
+      credits: 500,
+      desc: 'Best for business use.'
+    },
+    {
+      id: 'Business',
+      price: 250,
+      credits: 5000,
+      desc: 'Best for enterprise use.'
+    },
+  ]
