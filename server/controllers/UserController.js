c1458c03830c5e1f6cac5ca440fbc76187662b10
AdityaSinhasane <adityasinhasane100@gmail.com>
2024-10-29 14:05:38 +0530

    Third Commit

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,8 +1,17 @@
-
+import { Webhook }  from 'svix';
 
 // API Controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async(req,res) =>{
-    
+    try {   
+        // Create a Svix instance with clerk webhook secret.
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        await whook.verify(JSON.stringify(req.body))
+
+
+    } catch (error) {
+        
+    }
 }
